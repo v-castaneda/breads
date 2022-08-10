e2ff9357672a0ae11bedc2287cb2a9ea71b4e04d
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-08-10 19:52:31 -0400

    adding data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -4,7 +4,9 @@ const Bread = require("../models/bread.js");
 
 // INDEX
 breads.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", {
+    breads: Bread,
+  });
   //   res.send(Bread);
 });
 
--- views/index.jsx
+++ views/index.jsx
@@ -1,10 +1,17 @@
 const React = require("react");
 const Default = require("./layouts/Default");
 
-function Index() {
+function Index({ breads }) {
   return (
     <Default>
       <h2>Index Page</h2>
+      {/* This is a JSX comment. */}
+      {/* <p>I have {breads[0].name} bread!</p> */}
+      <ul>
+        {breads.map((bread, index) => {
+          return <li key={index}>{bread.name}</li>;
+        })}
+      </ul>
     </Default>
   );
 }
