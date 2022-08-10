451e5a39e08dc60ee6d1d077d0c8072dd709231f
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-08-10 19:16:48 -0400

    setting up Express-React-Views Middleware

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ require("dotenv").config();
 const PORT = process.env.PORT;
 const app = express();
 
+// MIDDLEWARE
+app.set("views", __dirname + "/views");
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
+
 // ROUTES
 app.get("/", (req, res) => {
   res.send("Welcome to an Awesome App about Breads!");
