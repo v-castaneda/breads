4119251181ab8433dc46612e2908a630504be3e5
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-08-13 09:52:01 -0400

    adding data to view

--- views/show.jsx
+++ views/show.jsx
@@ -2,10 +2,13 @@ const React = require("React");
 const Default = require("./layouts/default");
 
 function Show({ bread }) {
-  console.log(bread.name);
+  // Confirm we are getting our bread data in the terminal
+  //   console.log(bread.name);
   return (
     <Default>
       <h2>Show Page</h2>
+      <h3>{bread.name}</h3>
+      <img src={bread.image} alt={bread.name} />
     </Default>
   );
 }
