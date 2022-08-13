4fa8d7cd7c0592ca9d28be976342a496b0dfa54b
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-08-13 10:12:59 -0400

    adding CSS libraries - normalize and skeleton

--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -7,6 +7,18 @@ function Default(html) {
     <html>
       <head>
         <title>{html.title || "Default"}</title>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+          integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css"
+          integrity="sha512-EZLkOqwILORob+p0BXZc+Vm3RgJBOe1Iq/0fiI7r/wJgzOFZMlsqTa29UEl6v6U6gsV4uIpsNZoV32YZqrCRCQ=="
+          crossOrigin="anonymous"
+        />
       </head>
       <body>
         <div className="wrapper">
