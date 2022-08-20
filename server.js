2a98ad05985ecd460dc494eb2851c49f8e5b0f1b
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-08-20 10:30:43 -0400

    Integrated Mongoose

--- server.js
+++ server.js
@@ -2,11 +2,24 @@
 const express = require("express");
 const breads = require("./controllers/breads_controller");
 const methodOverride = require("method-override");
+const mongoose = require("mongoose");
 
 // CONFIGURATION
 require("dotenv").config();
 const PORT = process.env.PORT;
 const app = express();
+
+// DATABASE
+mongoose.connect(
+  process.env.MONGO_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("connected to mongo: ", process.env.MONGO_URI);
+  }
+);
 
 // MIDDLEWARE
 app.set("views", __dirname + "/views");
